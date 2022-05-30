eeaa50ed21af698158ebce872a95b1ee101a954b
Hyperbola <me@hyperbola.me>
2022-05-30 00:57:28 +0000

    remove some comments

--- src/util.ts
+++ src/util.ts
@@ -1,6 +1,4 @@
-// eslint-disable-next-line import/prefer-default-export
 export function xpathByText(tag: string, text: string): string {
-  // eslint-disable-next-line no-param-reassign
   text = text.replace(/"/g, '\\"')
   return `//${tag}[contains(text(), "${text}")]`
 }
