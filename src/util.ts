59a21b747078d76570031bcac338391d9c377693
hyperbola <me@hyperbola.me>
2023-04-16 15:12:38 +0800

    Bump to v1.2.1

--- src/util.ts
+++ src/util.ts
@@ -8,4 +8,4 @@ export function isValidPassword(p: string | undefined): boolean {
   return Boolean(p && p.length >= 8 && p.length <= 16)
 }
 
-export const version = '1.2.0'
+export const version = '1.2.1'
