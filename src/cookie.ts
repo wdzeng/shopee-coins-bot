9763d7b0f26ca139dadd9237d6141bda6c5158d9
hyperbola <me@hyperbola.me>
2023-07-22 01:52:51 +0800

    feat: implement v2
    Also upgrade dependencies and remove unused ones. Upgrade Node.js
    version to 20. Also update commands in package.json, and ESLint and
    prettier configurations.
    
    All unittests in v1 are removed.
    
    Removed VSCode devcontainer as it is no longer needed.

---
+++ src/cookie.ts
@@ -0,0 +1,15 @@
+import { InvalidCookieError } from './errors'
+
+export function parseCookie(cookie: string): Record<string, string> {
+  return cookie.split(';').reduce<Record<string, string>>((acc, cookieString) => {
+    const [key, value] = cookieString.split('=').map((s) => s.trim())
+    if (!key) {
+      throw new InvalidCookieError('Invalid cookie string: missing key')
+    }
+    if (!value) {
+      throw new InvalidCookieError('Invalid cookie string: missing value')
+    }
+    acc[key] = value
+    return acc
+  }, {})
+}
